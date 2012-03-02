59ce941d42effb33a6822849f2e88e517cef2394
Jonathan Fischoff <jonathan@fairauto.com>
2012-03-01 16:30:53 -0800

    Code clean up

--- src/Language/Haskell/TH/TypeSub.hs
+++ src/Language/Haskell/TH/TypeSub.hs
@@ -1,4 +1,19 @@
-module Language.Haskell.TH.TypeSub where
+-- | Subsitute one type for another in Template Haskell Dec's, Con's and Type's.
+-- | Warning! 
+-- | There are a few known issues. The types in cxt's are not subsistuted.
+-- | Additionally, the Dec's type variables are regenerated after subistution 
+-- | and all explicitly kinded type variables are converted to implicitly kinded type
+-- | variables.
+module Language.Haskell.TH.TypeSub (
+ subst_types_dec, 
+ sub_type_dec, 
+ sub_type_con, 
+ sub_type,
+ has_type,
+ get_cons,
+ get_con_types,
+ update_ty_vars,
+ Result) where
 import Language.Haskell.TH
 import Control.Arrow
 import Data.List
@@ -8,45 +23,81 @@ import Control.Monad
 import Debug.Trace.Helpers
 import Data.Tuple.Select
 
+-- | A result for partial functions
 type Result a = Either String a
 
---replace the binpath with just a type
-
 -- | Create a new data declaration where the type variables have been subsituted with the 
--- | supplied types.
---Todo
---this function can return an error. Should be an either
---all of the errors can be consolidating
---additionally I should make the other direction
-subst_types :: Dec -> [Type] -> Result Dec
-subst_types dec types  = do
-    let sub_type' dec (n, t) = sub_type dec (VarT n) t
+-- | supplied types. Returns an error if the more types the types are provided.
+subst_types_dec :: [Type] -> Dec -> Result Dec
+subst_types_dec types dec = do
+    let sub_type_dec' dec (n, t) = sub_type_dec t (VarT n) dec
     names <- mapM (get_ty_var_name dec) [0..length types - 1] 
-    foldM sub_type' dec $ zip names types 
+    return $ foldl' sub_type_dec' dec $ zip names types 
+    
+-- | Substitute the a new type for an existing type in all the constructors in a Dec.
+-- | If the type to replace is missing, the function does nothing. 
+sub_type_dec :: Type -> Type -> Dec -> Dec 
+sub_type_dec new_type old_type dec = update_ty_vars $ 
+    modify_cons dec (map (sub_type_con new_type old_type)) 
+    
+-- | Substitute the new type for the old type in the constructor       
+sub_type_con :: Type -> Type -> Con -> Con
+sub_type_con new_type old_type con = modify_types con (map (sub_type new_type old_type))
+
+-- | Substitute the new type for the old type in the type  
+-- transform is from Uniplate     
+sub_type :: Type -> Type -> Type -> Type
+sub_type new_type old_type input = transform sub_type_type' input where
+     sub_type_type' t | t == old_type = new_type
+     sub_type_type' x | otherwise = x
+
+------------------------------------------------------------------------------------------------
+--Various helper functions that will get moved somewhere else eventually
 
 ty_var_name :: TyVarBndr -> Name
 ty_var_name (KindedTV name _ ) = name
 ty_var_name (PlainTV name) = name
 
 modify_cons :: Dec -> ([Con] -> [Con]) -> Dec
-modify_cons (NewtypeD x y z con w) f = result where
-     h = f [con]
-     result = (NewtypeD x y z (head h) w)
-modify_cons (DataD x y z cons w)   f = result where
-    cs = f cons
-    result = (DataD x y z cs w)
-    
+modify_cons (NewtypeD x y z con w) f     = NewtypeD x y z (head $ f [con]) w
+modify_cons (DataD x y z cons w)   f     = DataD x y z (f cons) w
+modify_cons (DataInstD x y z cons w) f   = DataInstD x y z (f cons) w
+modify_cons (NewtypeInstD x y z con w) f = NewtypeInstD x y z (head $ f [con]) w
+modify_cons x _                          = x
+
 get_cons :: Dec -> [Con]
-get_cons (NewtypeD x y z con w) = [con]
-get_cons (DataD x y z cons w)   = cons
-
-set_cons :: Dec -> [Con] -> Dec
-set_cons (NewtypeD x y z _ w) [con] = (NewtypeD x y z con w) 
-set_cons (DataD x y z _ w) cons = (DataD x y z cons w)  
-
+get_cons (NewtypeD _ _ _ con _)      = [con]
+get_cons (DataD _ _ _ cons _)        = cons
+get_cons (DataInstD _ _ _ cons _)    = cons
+get_cons (NewtypeInstD _ _ _ con _)  = [con]
+get_cons _                           = []
+ 
 get_ty_vars :: Dec -> [TyVarBndr]
 get_ty_vars (NewtypeD _ _ ty_vars _ _) = ty_vars
 get_ty_vars (DataD    _ _ ty_vars _ _) = ty_vars
+get_ty_vars (TySynD _ ty_vars _)       = ty_vars
+get_ty_vars (ClassD _ _ ty_vars _ _)   = ty_vars
+get_ty_vars (FamilyD _ _ ty_vars _ )   = ty_vars
+get_ty_vars _                          = []
+
+set_ty_vars :: Dec -> [TyVarBndr] -> Dec
+set_ty_vars (NewtypeD x y _ z w)  ty_vars = NewtypeD x y ty_vars z w
+set_ty_vars (DataD    x y _ z w)  ty_vars = DataD x y ty_vars z w
+set_ty_vars (TySynD x _ y)        ty_vars = TySynD x ty_vars y
+set_ty_vars (ClassD x y _ z w)    ty_vars = ClassD x y ty_vars z w
+set_ty_vars (FamilyD x y _ z )    ty_vars = FamilyD x y ty_vars z 
+set_ty_vars x _                           = x
+
+
+get_type_name :: Type -> Result Name
+get_type_name (ForallT _ _ typ) = get_type_name typ
+get_type_name (VarT n)          = Right n
+get_type_name (ConT n)          = Right n
+get_type_name x                 = Left ("No name for " ++ show x)
+
+from_right :: Result a -> a
+from_right (Right x) = x
+from_right (Left x)  = error $ x ++ " is not Right!"
 
 get_ty_var_name :: Dec -> Int -> Result Name
 get_ty_var_name dec i = ty_var_name <$> get_value (get_ty_vars dec) i
@@ -55,29 +106,21 @@ get_value :: [a] -> Int -> Result a
 get_value xs i | i < length xs = Right $ xs !! i
 get_value xs i | otherwise     = Left "Index out of bounds"
 
-sub_type :: Dec -> Type -> Type -> Result  Dec 
-sub_type dec typ typ' = Right $ modify_cons dec (map (sub_type_con typ typ')) 
+collect_vars :: Type -> [Type]
+collect_vars typ = [VarT n | VarT n <- universe typ]
 
-thirdM :: Monad m => (t -> m t3) -> (t1, t2, t) -> m (t1, t2, t3) 
-thirdM f (x,y,z) = do
-    z' <- f z
-    return (x,y, z')
+make_ty_vars :: Type -> [TyVarBndr]
+make_ty_vars = map (PlainTV . from_right . get_type_name) . nub . collect_vars
 
-secondM :: Monad m => (t -> m t2) -> (t1, t) -> m (t1, t2) 
-secondM f (x,y) = do
-    y' <- f y
-    return (x,y')
+update_ty_vars :: Dec -> Dec 
+update_ty_vars (TySynD n _ t) = (TySynD n (make_ty_vars t) t)
+update_ty_vars dec = set_ty_vars dec $ concatMap make_ty_vars $ 
+    concatMap (get_con_types) $ get_cons dec
 
-firstM :: Monad m => (t -> m t1) -> (t, t2) -> m (t1, t2) 
-firstM f (x,y) = do
-    x' <- f x
-    return (x', y)
     
+third :: (c -> d) -> (a, b, c) -> (a, b, d)
 third f (x, y, z) = (x, y, f z)
     
-zipM t = fmap (\(x, y) -> zip x y) t
-zipM3 t = fmap (\(x, y, z) -> zip3 x y z) t
-
 get_con_types :: Con -> [Type]
 get_con_types (NormalC n st) = map snd st
 get_con_types (RecC n st) = map sel3 st
@@ -89,33 +132,18 @@ modify_types (NormalC n strict_types)  f = NormalC n $ uncurry zip $ (second f $
 modify_types (RecC n var_strict_types) f = RecC    n $ (\(x, y, z) -> zip3 x y z) (third f $ unzip3 var_strict_types )
 modify_types (InfixC x n y) f = result where
     [x', y'] = uncurry zip $ second f $ unzip [x, y]
-
     result = InfixC x' n y'
 modify_types (ForallC t cxt con) f = ForallC t cxt $ modify_types con f
-    
-sub_type_con :: Type -> Type -> Con -> Con
-sub_type_con typ typ' con = modify_types con (map (sub_type_type typ typ'))
-    
+       
 has_var :: Name -> Type -> Bool
 has_var name typ = any (name==)  [ n | VarT n <- universe typ]
-
-sub_type_type :: Type -> Type -> Type -> Type
-sub_type_type ty typ old_typ = transform sub_type_type' old_typ where
-    sub_type_type' t | t == ty = typ
-    sub_type_type' x | otherwise = x
 
 has_type :: Type -> Type -> Bool
 has_type typ_to_find typ = any (typ_to_find==) $ universe typ
                   
-collect_type_args :: Type -> [Type]
-collect_type_args (AppT x y) = x:(collect_type_args y)
-collect_type_args x          = [x]
 
-reduce_type :: Name -> Type -> Q (Result Dec)
-reduce_type name app_tys = do
-    TyConI dec <- reify name
-    let args = collect_type_args app_tys
-    return $ subst_types dec args
+
+
 
 
 
--- tests/Main.hs
+++ tests/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TupleSections #-}
 module Main where
 import Language.Haskell.TH
 import Language.Haskell.TH.TypeSub
@@ -12,101 +13,114 @@ import Language.Haskell.TH.Instances
 import Test.QuickCheck.Checkers
 import Data.List
 import Data.Generics.Uniplate.Data
+import Control.Applicative ((<$>))
     
-main = defaultMainWithArgs tests ["-a 100", "-o 4"]
-
---I need to test each function with every possible constructor type
---I should test that any type that is one I handle, does not cause a crash
+main = defaultMainWithArgs tests ["-a 100", "-o 5"]
 
 tests = [
-            testGroup "collect_type" [
-                testCase "test_collect_type_args_0" test_collect_type_args_0,
-                testCase "test_collect_type_args_1" test_collect_type_args_1,
-                testCase "test_collect_type_args_2" test_collect_type_args_2,
-                testProperty "prop_collect_unfolds_0" prop_collect_unfolds_0
+            testGroup "sub_type" [
+                testCase "test_sub_type" test_sub_type_0,
+                testCase "test_sub_type_1" test_sub_type_1,
+                testCase "test_sub_type_2" test_sub_type_2,
+                testCase "test_sub_type_3" test_sub_type_3,
+                testCase "test_sub_type_4" test_sub_type_4,
+                testProperty "prop_sub_type" prop_abstract_to_subs
             ],
-            testGroup "sub_type_type" [
-                testCase "test_sub_type_type" test_sub_type_type_0,
-                testCase "test_sub_type_type_1" test_sub_type_type_1,
-                testProperty "prop_sub_type_type" prop_abstract_to_subs,
-                testProperty "prop_sub_type_type_con" prop_abstract_to_subs_con
+            testGroup "sub_type_con" [
+                testCase "test_sub_type_con_0" test_sub_type_con_0,
+                testProperty "prop_sub_type_con" prop_abstract_to_subs_con
+            ],
+            testGroup "sub_type_dec" [
+                testCase "test_sub_type_dec_0" test_sub_type_dec_0,
+                testProperty "prop_sub_type_dec" prop_abstract_to_subs_dec
+            ],
+            testGroup "sub_types_dec" [
+                testCase "test_sub_types_dec_0" test_sub_types_dec_0
             ]
 
     ]
       
-test_collect_type_args_0 = actual @?= expected where
-    actual   = collect_type_args $ foldr1 AppT expected
-    expected = [AppT (ConT $ mkName "Test") (VarT $ mkName "x"), ConT $ mkName "MoreTest"]
-   
-test_collect_type_args_1 = actual @?= expected where
-    actual   = collect_type_args $ foldr1 AppT expected
-    expected = [UnboxedTupleT 0, TupleT 3, VarT $ mkName "iz"]
-    
-test_collect_type_args_2 = 
-    assertBool "test_collect_type_args_2" (actual `app_type_iso` expected) where
-        actual   = collect_type_args $ foldr1 AppT expected
-        expected = [AppT (VarT $ mkName "rx") (VarT $ mkName "ykqtujsxvcrciouq")]
-
-    
-prop_collect_unfolds_0 :: [Type] -> Property
-prop_collect_unfolds_0 xs = length xs > 0 ==> 
-    app_type_iso xs $ collect_type_args $ foldr1 AppT xs
-
-app_type_iso :: [Type] -> [Type] -> Bool
-app_type_iso xs ys | length xs == length ys = xs == ys
-app_type_iso xs ys | length xs > 1 && xs > ys = all_but_last_two ys xs
-app_type_iso xs ys | length ys > 1 && ys > xs = all_but_last_two xs ys
-app_type_iso xs ys | otherwise = False
-
-all_but_last_two xs ys = result where
-    last_one             = last xs
-    first_part_x         = reverse $ drop 1 $ reverse xs
-    last_unfolded        = collect_type_args last_one
-    
-    last_other           = reverse $ take (length last_unfolded) $ reverse ys
-    first_part_y         = reverse $ drop (length last_unfolded) $ reverse ys
-    
-    
-    result = (last_unfolded == last_other && first_part_x == first_part_y)
-    
 ------------------------------------------------------------------------------
 
-test_sub_type_type_0 = actual @?= expected where
-    actual   = sub_type_type (VarT $ mkName "a") (VarT $ mkName "test") 
+test_sub_type_0 = actual @?= expected where
+    actual   = sub_type (VarT $ mkName "test") (VarT $ mkName "a")
                     (AppT (VarT $ mkName "a") (AppT (VarT $ mkName "b") (VarT $ mkName "c")))
     expected = AppT (VarT $ mkName "test") (AppT (VarT $ mkName "b") (VarT $ mkName "c"))
 
-test_sub_type_type_1 = actual @?= expected where
-    actual   = sub_type_type (UnboxedTupleT (-1)) (VarT $ mkName "") (UnboxedTupleT (-1)) 
+test_sub_type_1 = actual @?= expected where
+    actual   = sub_type (VarT $ mkName "") (UnboxedTupleT (-1)) (UnboxedTupleT (-1)) 
     expected = VarT $ mkName ""
 
-{-    
-test_sub_type_type_1 = actual @?= expected where
-    actual   = sub_type_type (UnboxedTupleT (-1)) (VarT $ mkName "") (ForallT [] [] (VarT test)) 
-    expected = VarT $ mkName ""
--}
     
+test_sub_type_2 = actual @?= expected where
+    actual   = sub_type ListT ArrowT $ sub_type ArrowT ListT (ForallT [] [] ListT)
+    expected = (ForallT [] [] ListT)
+
+test_sub_type_3 = actual @?= expected where
+    actual   = sub_type (AppT ArrowT ArrowT) ArrowT $ 
+               sub_type ArrowT (AppT ArrowT ArrowT) expected
+    expected = AppT (AppT (UnboxedTupleT 27799) (UnboxedTupleT (-41693))) (AppT ArrowT ArrowT)    
+    
+test_sub_type_4 = actual @?= expected where
+    actual   = sub_type (AppT (TupleT 12) (ForallT [] [] ListT)) ListT $
+               sub_type ListT (AppT (TupleT 12) (ForallT [] [] ListT)) expected
+    expected = AppT (SigT (AppT (ConT $ mkName "Y24li12988h5rgE7kC") (TupleT 7)) 
+                    (ArrowK StarK StarK)) 
+                    (AppT (TupleT 12) (ForallT [] [] ListT))
+                    
+
+test_sub_type_con_0  = actual @?= expected where
+    actual   = sub_type_con new_type old_type constr
+    expected = NormalC (mkName "hey") $ map (NotStrict,) [new_type, VarT $ mkName "w", new_type]
+    constr   = NormalC (mkName "hey") $ map (NotStrict,) [old_type, VarT $ mkName "w", old_type]
+    old_type = AppT (ConT $ mkName "Int") (VarT $ mkName "a")
+    new_type = ArrowT
+    
+test_sub_type_dec_0  = actual @?= expected where
+    actual        = sub_type_dec new_type old_type dec
+    expected      = DataD [] name [PlainTV $ mkName "a"] expected_cons 
+    dec           = DataD [] name [] cons
+    expected_cons = undefined
+    cons          = undefined
+    old_type      = AppT (ConT $ mkName "Int") (VarT $ mkName "a")
+    new_type      = TupleT 10        
+         
+test_sub_types_dec_0 = undefined
+
 fromRight (Right x) = x
 
 error_left (Right x) = x
 error_left (Left x) = error x
 
-prop_abstract_to_subs :: (Type, Type) -> Gen Bool
-prop_abstract_to_subs (typ, old_typ) = do
+traceItNote x y = trace (x ++ show y) y
+
+prop_abstract_to_subs :: Gen Bool
+prop_abstract_to_subs = do 
+    old_typ <- arbitrary
+    typ  <- suchThat arbitrary (not . (flip has_type) old_typ)
     typ' <- elements $ universe old_typ
-    let abstracted = sub_type_type typ' typ old_typ
-        subbed     = sub_type_type typ typ' abstracted    
+    let abstracted = sub_type typ typ' old_typ
+        subbed     = sub_type typ' typ abstracted    
     return $ old_typ == subbed
 
-prop_abstract_to_subs_con :: (Type, Con) -> Gen Bool
-prop_abstract_to_subs_con (typ, con) = do
+prop_abstract_to_subs_con :: Gen Bool
+prop_abstract_to_subs_con = do
+    con <- arbitrary
+    typ  <- suchThat arbitrary (\x -> all (not . (has_type x)) (get_con_types con))
     typ' <- elements $ concatMap universe $ get_con_types con
-    let abstracted = sub_type_con typ' typ con
-        subbed     = sub_type_con typ typ' abstracted    
+    let abstracted = sub_type_con typ typ' con
+        subbed     = sub_type_con typ' typ abstracted    
     return $ con == subbed 
 
 
-
+prop_abstract_to_subs_dec :: Gen Bool
+prop_abstract_to_subs_dec = do
+    dec <- update_ty_vars <$> haskell_98_data_dec primitive_types
+    typ  <- suchThat arbitrary (\x -> all (not . (has_type x)) (concatMap get_con_types $ get_cons dec))
+    typ' <- elements $ concatMap universe $ concatMap get_con_types $ get_cons dec
+    let abstracted = sub_type_dec typ typ' dec
+        subbed     = sub_type_dec typ' typ abstracted
+    return $ dec == subbed
 
 
 
